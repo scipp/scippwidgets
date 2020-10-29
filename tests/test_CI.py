51cb6da8c10c335e2afc92520638be3f6f027d20
Matthew-Andrew <matthew.andrew@tessella.com>
2020-10-29 11:52:36 +0000

    Add dummy test

---
+++ tests/test_CI.py
@@ -0,0 +1,8 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (c) 2020 Scipp contributors (https://github.com/scipp)
+# @file
+# @author Matthew Andrew
+
+
+def test_CI_stack():
+    assert True
