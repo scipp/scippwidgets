48a888584f93d2c34daa29e4346cb857b186af00
Matthew-Andrew <matthew.andrew@tessella.com>
2020-11-14 09:40:42 +0000

    Corrected faling unit tests

--- tests/test_inputs.py
+++ tests/test_inputs.py
@@ -10,10 +10,10 @@ def test_InputSpec_creates_widget_with_correct_properties():
     name = 'test_input'
     options = ('x', 'y', 'z')
     tooltip = 'input here'
-    input_spec = StringInputSpec(name=name, options=options, tooltip=tooltip)
+    input_spec = StringInputSpec(function_arg_name=name,
+                                 options=options,
+                                 tooltip=tooltip)
     input_spec.widget.value = 'user input'
-
-    input_widget = input_spec.create_input_widget()
 
     assert input_spec.widget.options == options
     assert input_spec.widget.placeholder == tooltip
@@ -23,7 +23,7 @@ def test_InputSpec_creates_widget_with_correct_properties():
 def test_InputSpec_evaluates_expression_in_scope():
     name = 'test_input'
     scope = {'test_obj': [1, 2, 3, 45]}
-    input_spec = InputSpec(name=name, scope=scope)
+    input_spec = InputSpec(function_arg_name=name, scope=scope)
     input_spec.widget.value = 'test_obj'
 
     assert input_spec.function_arguments == {'test_input': [1, 2, 3, 45]}
