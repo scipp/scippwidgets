dc4e1250a15f69055d1f376e80231f80ac46b7ad
Matthew-Andrew <matthew.andrew@tessella.com>
2020-11-16 15:05:36 +0000

    Adding different param tests

--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -4,6 +4,7 @@
 # @author Matthew Andrew
 
 from scipp_widgets.widgets import (DisplayWidget, ProcessWidget)
+from scipp_widgets.input_spec import StringInputSpec
 import pytest
 
 
@@ -39,3 +40,19 @@ def test_can_create_and_run_process_widget():
     widget._on_button_clicked(0)
 
     assert scope['obj_name'] == 'func_return'
+
+
+def test_function_with_args_and_kwargs():
+    def test_func(p_or_k='io', *args, k_only='k_only', **kwargs):
+        return str(p_or_k) + str(args) + str(k_only) + str(kwargs)
+
+    scope = {'test': 'one'}
+
+    widget = ProcessWidget(
+        test_func, [StringInputSpec('p_or_k'),
+                    StringInputSpec('k_only')],
+        scope=scope)
+    widget.output.value = 'obj_name'
+    widget._on_button_clicked(0)
+
+    assert scope['obj_name'] == ''
